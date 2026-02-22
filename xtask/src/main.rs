edd932f73d11752ffe4bc17af24926ed2fdb3447
spindlymist <ocrobin@gmail.com>
2026-02-22 11:23:21 -0800

    New and improved xtasks

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -27,81 +27,134 @@ struct Cli {
 
 #[derive(Subcommand, Clone)]
 enum Task {
-    MakeSeeds(MakeSeedsArgs)
+    MakeSeeds(MakeSeedsArgs),
+    Render(RenderArgs),
 }
 
 #[derive(Args, Clone)]
 struct MakeSeedsArgs {
+    /// How many seeds per level to create
     #[arg(short, default_value = "3")]
     n: usize,
+    #[arg(short, long)]
+    /// Pick new random seeds instead of reusing old ones
+    replace: bool,
+    /// Glob pattern for level directory names (relative to Worlds)
     #[arg(default_value = "*")]
     glob: String,
+}
+
+#[derive(Args, Clone)]
+struct RenderArgs {
+    /// Glob pattern for level directory names (relative to Worlds)
+    #[arg(default_value = "*")]
+    glob: String,
+    /// Path to output directory
+    #[arg(default_value = "output")]
+    output_dir: PathBuf,
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
     match cli.task {
         Task::MakeSeeds(args) => make_seeds(args),
+        Task::Render(args) => render(args),
     }
 }
 
-#[derive(Serialize, Deserialize)]
+fn list_level_names(glob: String) -> Result<Vec<String>> {
+    if glob.contains(['/', '\\']) {
+        bail!("Glob pattern should not contain a slash");
+    }
+    let glob = glob::glob(&glob)?;
+    
+    let current_dir = env::current_dir()?;
+    env::set_current_dir(WORLDS_DIR.as_path())?;
+    
+    let mut level_names = Vec::<String>::new();
+    for path in glob {
+        let path = path?;
+        if path.is_dir()
+            && let Some(level_name) = path.to_str()
+        {
+            level_names.push(level_name.to_owned());
+        }
+    }
+    
+    env::set_current_dir(current_dir)?;
+    Ok(level_names)
+}
+
+#[derive(Serialize, Deserialize, Default)]
 struct SeedIndexEntry {
     seeds: Vec<MapSeed>,
 }
 
-fn make_seeds(args: MakeSeedsArgs) -> Result<()> {
-    if args.glob.contains(['/', '\\']) {
-        bail!("Glob pattern should not contain a slash");
-    }
-    let glob = glob::glob(&args.glob)?;
-    
-    let level_names = {
-        let current_dir = env::current_dir()?;
-        env::set_current_dir(WORLDS_DIR.as_path())?;
-        
-        let mut level_names = Vec::<String>::new();
-        for path in glob {
-            let path = path?;
-            if path.is_dir()
-                && let Some(level_name) = path.to_str()
-            {
-                level_names.push(level_name.to_owned());
-            }
-        }
-        
-        env::set_current_dir(current_dir)?;
-        level_names
-    };
-    
-    let mut seed_index: BTreeMap<String, SeedIndexEntry> = {
-        if SEED_INDEX_PATH.exists() {
+fn load_seed_index() -> Result<BTreeMap<String, SeedIndexEntry>> {
+    let seed_index = if SEED_INDEX_PATH.exists() {
             let contents = std::fs::read_to_string(SEED_INDEX_PATH.as_path())?;
             toml::from_str(&contents)?
         }
         else {
             BTreeMap::new()
-        }
-    };
+        };
+    Ok(seed_index)
+}
+
+fn make_seeds(args: MakeSeedsArgs) -> Result<()> {
+    let level_names = list_level_names(args.glob)?;
+    let mut seed_index = load_seed_index()?;
     
     for level_name in level_names {
+        let index_entry = seed_index.entry(level_name.clone())
+            .or_insert_with(|| SeedIndexEntry::default());
+        let seeds = &mut index_entry.seeds;
+        
+        if args.replace {
+            seeds.clear();
+        }
+        else {
+            seeds.truncate(args.n);
+        }
+        
+        while seeds.len() < args.n {
+            seeds.push(MapSeed::random());
+        }
+        
         let level_dir = WORLDS_DIR.join(&level_name);
         let output_dir = SEEDS_DIR.join(&level_name);
-        let seeds: Vec<_> = (0..args.n).map(|_| MapSeed::random()).collect();
-        
         if output_dir.exists() {
             std::fs::remove_dir_all(&output_dir)?;
         }
         std::fs::create_dir_all(&output_dir)?;
-        render_seeds(&level_dir, &seeds, &output_dir);
         
-        seed_index.insert(level_name, SeedIndexEntry {
-            seeds,
-        });
+        render_seeds(&level_dir, &seeds, &output_dir);
     }
     
     let seed_index_serialized = toml::to_string_pretty(&seed_index)?;
     fs::write(SEED_INDEX_PATH.as_path(), seed_index_serialized)?;
+    
+    Ok(())
+}
+
+fn render(args: RenderArgs) -> Result<()> {
+    let level_names = list_level_names(args.glob)?;
+    let seed_index = load_seed_index()?;
+    
+    for level_name in level_names {
+        let Some(index_entry) = seed_index.get(&level_name) else {
+            continue
+        };
+        
+        let level_dir = WORLDS_DIR.join(&level_name);
+        let output_dir = args.output_dir.join(&level_name);
+        if output_dir.exists() {
+            std::fs::remove_dir_all(&output_dir)?;
+        }
+        std::fs::create_dir_all(&output_dir)?;
+        
+        render_seeds(&level_dir, &index_entry.seeds, &output_dir);
+    }
     
     Ok(())
 }
