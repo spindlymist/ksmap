b5c32ee2b666dcd0442839bc0adbf787f64f48a2
spindlymist <ocrobin@gmail.com>
2026-06-26 07:45:00 -0700

    Parameterize grid line thickness

--- gui/src/map_widget.rs
+++ gui/src/map_widget.rs
@@ -33,12 +33,15 @@ pub fn build_map(
         [0.5, 0.0, 0.5, 1.0],
     ];
     
-    let cell_width: f32 = 10.0;
-    let cell_height: f32 = 10.0;
+    let cell_width = 10.0f32;
+    let cell_height = 10.0f32;
+    let line_thickness = 1.0f32;
+    let cell_width_outer = cell_width + line_thickness;
+    let cell_height_outer = cell_height + line_thickness;
 
     let [width_avail, height_avail] = ui.get_content_region_avail();
-    let cols = (width_avail / (cell_width + 1.0)).ceil() as i32;
-    let rows = (height_avail / (cell_height + 1.0)).ceil() as i32;
+    let cols = (width_avail / (cell_width_outer)).ceil() as i32;
+    let rows = (height_avail / (cell_height_outer)).ceil() as i32;
     
     // Pan
     if ui.is_mouse_released(MouseButton::Right) {
@@ -51,8 +54,8 @@ pub fn build_map(
     }
     if let Some((origin_x, origin_y)) = &map_state.drag_origin {
         let [dx, dy] = ui.mouse_drag_delta(MouseButton::Right);
-        let new_x = origin_x - (dx / (cell_width + 1.0)) as i32;
-        let new_y = origin_y - (dy / (cell_height + 1.0)) as i32;
+        let new_x = origin_x - (dx / (cell_width_outer)) as i32;
+        let new_y = origin_y - (dy / (cell_height_outer)) as i32;
         map_state.center = (new_x, new_y);
     }
     
@@ -65,13 +68,15 @@ pub fn build_map(
     let draw_list = ui.get_window_draw_list();
     let [origin_x, origin_y] = ui.get_cursor_screen_pos();
     
-    for x_grid in 1..=cols {
-        let x_screen = (origin_x - 1.0) + (x_grid as f32) * (cell_width + 1.0);
-        draw_list.add_line_v(x_screen, origin_y, origin_y + height_avail, [0.1, 0.1, 0.1], 1.0);
-    }
-    for y_grid in 1..=rows {
-        let y_screen = (origin_y - 1.0) + (y_grid as f32) * (cell_height + 1.0);
-        draw_list.add_line_h(origin_x, origin_x + width_avail, y_screen, [0.1, 0.1, 0.1], 1.0);
+    if line_thickness > 0.0 {
+        for x_grid in 1..=cols {
+            let x_screen = (origin_x - line_thickness) + (x_grid as f32) * (cell_width_outer);
+            draw_list.add_line_v(x_screen, origin_y, origin_y + height_avail, [0.1, 0.1, 0.1], line_thickness);
+        }
+        for y_grid in 1..=rows {
+            let y_screen = (origin_y - line_thickness) + (y_grid as f32) * (cell_height_outer);
+            draw_list.add_line_h(origin_x, origin_x + width_avail, y_screen, [0.1, 0.1, 0.1], line_thickness);
+        }
     }
     
     for y in y_min..y_max {
@@ -84,8 +89,8 @@ pub fn build_map(
             let dy = y - y_min;
             
             let top_left = [
-                origin_x + (dx as f32) * (cell_width + 1.0),
-                origin_y + (dy as f32) * (cell_height + 1.0),
+                origin_x + (dx as f32) * (cell_width_outer),
+                origin_y + (dy as f32) * (cell_height_outer),
             ];
             let bottom_right = [
                 top_left[0] + cell_width,
@@ -112,12 +117,12 @@ pub fn build_map(
         let bottom_right_dx = bounds.x.end as i32 - x_min;
         let bottom_right_dy = bounds.y.end as i32 - y_min;
         let top_left = [
-            origin_x + top_left_dx as f32 * (cell_width + 1.0) - 1.0,
-            origin_y + top_left_dy as f32 * (cell_height + 1.0) - 1.0,
+            origin_x + top_left_dx as f32 * (cell_width_outer) - line_thickness,
+            origin_y + top_left_dy as f32 * (cell_height_outer) - line_thickness,
         ];
         let bottom_right = [
-            origin_x + bottom_right_dx as f32 * (cell_width + 1.0),
-            origin_y + bottom_right_dy as f32 * (cell_height + 1.0),
+            origin_x + bottom_right_dx as f32 * (cell_width_outer),
+            origin_y + bottom_right_dy as f32 * (cell_height_outer),
         ];
         draw_list.add_rect(top_left, bottom_right, [1.0, 1.0, 1.0, 1.0])
             .filled(false)
@@ -131,14 +136,14 @@ pub fn build_map(
     let [mouse_x, mouse_y] = ui.mouse_pos();
     let dx = mouse_x - origin_x;
     let dy = mouse_y - origin_y;
-    let grid_x = (dx / (cell_width + 1.0)) as i32;
-    let grid_y = (dy / (cell_height + 1.0)) as i32;
+    let grid_x = (dx / (cell_width_outer)) as i32;
+    let grid_y = (dy / (cell_height_outer)) as i32;
     let screen_x = x_min + grid_x;
     let screen_y = y_min + grid_y;
     
     let hover_top_left = [
-        origin_x + grid_x as f32 * (cell_width + 1.0),
-        origin_y + grid_y as f32 * (cell_height + 1.0),
+        origin_x + grid_x as f32 * (cell_width_outer),
+        origin_y + grid_y as f32 * (cell_height_outer),
     ];
     let hover_bottom_right = [
         hover_top_left[0] + cell_width,
